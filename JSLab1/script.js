17c56d9d57160a95e7af2328ced4aa3c6c726319
Riley Stamper <rileystamper2@gmail.com>
2022-10-12 22:30:20 -0400

    JSLab1

---
+++ JSLab1/script.js
@@ -0,0 +1,21 @@
+let firstName = "Riley";
+let lastName = "Stamper";
+let fullName = (firstName + " " + lastName);
+
+console.log (fullName);
+
+let population = (672351);
+console.log (population);
+console.log (population * 3)
+
+let javaScriptIsCool = true;
+
+let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+for (let a = 1; a <= 5; a++) {
+    console.log (a);
+}
+
+for (let b = 0; b <= 4; b++) {
+    console.log (b);
+}
