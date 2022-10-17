914b68fb7a52550ee910053efafc0eccf334cbc9
rstamper6 <rileystamper2@gmail.com>
2022-10-17 02:17:28 -0400

    finish

--- longestname/script.js
+++ longestname/script.js
@@ -1,31 +1,25 @@
-let name1 = "Toony";
-let name2 = "Tommy";
-let name3 = "titty";
+let name1 = "Riley";
+let name2 = "Tony";
+let name3 = "Sam";
 
-
-let longestName;
-if (name1.length > name2.length && name1.length > name3.length)
-console.log (`${name1} is the longest name.`);
-
-else if (name2.length > name1.length && name2.length > name3.length)
-console.log (`${name2} is the longest name.`);
-
-else if (name3.length > name1.length && name3.length > name2.length)
-console.log (`${name3}  is the longest name.`);
-
-else if (name1.length === name2.length)
-console.log (`${name1} and ${name2} tie for the longest name.`);
-
-else if (name1.length === name3.length)
-console.log (`${name1} and ${name3} tie for the longest name.`);
-
-else if (name2.length === name3.length)
-console.log (`${name2} and ${name3} tie for the longest name.`);
-
-else if (name1.length === name2.length && name2.length === name3.length)
-console.log (`All three names, ${name1}, ${name2}, and ${name3}, are the same length.`);
-
-
-
-
-
+if (name1.length > name2.length && name1.length > name3.length){
+    console.log (`${name1} is the longest name.`)
+}
+else if (name2.length > name1.length && name2.length > name3.length){
+    console.log (`${name2} is the longest name.`)
+}
+else if (name3.length > name1.length && name3.length > name2.length){
+    console.log (`${name3}  is the longest name.`)
+}
+else if (name1.length === name2.length && !(name1.length === name3.length)){
+    console.log (`${name1} and ${name2} tie for the longest name.`)
+}
+else if (name1.length === name3.length && !(name1.length === name2.length)){
+    console.log (`${name1} and ${name3} tie for the longest name.`)
+}
+else if (name2.length === name3.length && !(name2.length === name1.length)){
+    console.log (`${name2} and ${name3} tie for the longest name.`)
+}
+else {
+    console.log (`All three names, ${name1}, ${name2}, and ${name3}, are the same length.`)
+}
