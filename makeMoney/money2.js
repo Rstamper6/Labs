f22e07a4465f567b1fc06ee1fbb6267e1dcb12a6
Riley Stamper <rileystamper2@gmail.com>
2022-11-07 18:30:14 -0500

    update

--- makeMoney/money2.js
+++ makeMoney/money2.js
@@ -1,9 +1,11 @@
-let form = document.getElementById('coinForm')
-let howMany = document.getElementById('howMany')
-let quarter = document.getElementById('quarter')
-let dime = document.getElementById('dime')
-let nickel = document.getElementById('nickel')
-let penny = document.getElementById('penny')
+let form = document.getElementById('coinForm');
+let howMany = document.getElementById('howMany');
+let totalText = document.querySelector("h2");
+
+let quarter = document.getElementById('quarter');
+let dime = document.getElementById('dime');
+let nickel = document.getElementById('nickel');
+let penny = document.getElementById('penny');
 
 let qp = .25;
 let dp = .10;
@@ -12,7 +14,6 @@ let pp = .01;
 
 
 let imgDiv = document.getElementById('coinPics')
-
 imgDiv.addEventListener('click', e =>{
     e.target.remove();
 })
@@ -21,7 +22,8 @@ imgDiv.addEventListener('click', e =>{
 form.addEventListener('submit', e=> {
     e.preventDefault()
     // console.log(howMany.value);
-    
+    let total = 0;
+
     if(quarter.checked){
         for(let i = 0; i < howMany.value; i++){
             let img = document.createElement('img');
@@ -30,6 +32,8 @@ form.addEventListener('submit', e=> {
             img.classList.add("image")
             document.getElementById('coinPics').appendChild(img)
             }
+        total = qp * howMany.value
+        console.log(total);
         console.log(`Quarter is checked and ${howMany.value} were added`);
     }
     else if(dime.checked){
@@ -40,6 +44,8 @@ form.addEventListener('submit', e=> {
             img.classList.add("image")
             document.getElementById('coinPics').appendChild(img)
             }
+        total = dp * howMany.value
+        console.log(total);
         console.log(`Dime is checked and ${howMany.value} were added`);
     }
     else if(nickel.checked){
@@ -50,6 +56,8 @@ form.addEventListener('submit', e=> {
             img.classList.add("image")
             document.getElementById('coinPics').appendChild(img)
             }
+        total = np * howMany.value
+        console.log(total);
         console.log(`Nickel is checked and ${howMany.value} were added`);
     }
     else if(penny.checked){
@@ -60,8 +68,12 @@ form.addEventListener('submit', e=> {
             img.classList.add("image")
             document.getElementById('coinPics').appendChild(img)
             }
+        total = pp * howMany.value
+        console.log(total);
         console.log(`Penny is checked and ${howMany.value} were added`);
     }
+
+    totalText.innerText = `Your total is $${total + total}`
 
 
 
