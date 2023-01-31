04d6ec36856e238b776cdd35b36de7e5f2804600
Rstamper6 <rileystamper2@gmail.com>
2023-01-30 22:07:31 -0500

    update

---
+++ socialPosts/src/components/SocialPosts.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import { Post } from '../models/Post';
+
+
+export function SocialPosts () {
+    const [posts, setPosts] = useState<Post[]>([])
+
+  return (
+    <div>
+      {}
+    </div>
+  );
+}
